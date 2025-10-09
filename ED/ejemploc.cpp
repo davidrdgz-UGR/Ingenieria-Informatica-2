ac194923cb053f808b9bdf6690d94a4731dbb043
davidrdgz-UGR <e.davidrdgz@go.ugr.es>
2025-10-09 19:25:48 +0200

    probando c++

---
+++ ED/ejemploc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // Librería para entrada/salida
+
+using namespace std;
+
+int main() {
+    cout << "Hola, mundo!" << endl; // Imprime en consola
+    return 0; // Fin del programa
+}
+
